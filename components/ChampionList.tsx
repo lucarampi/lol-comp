82db60847c85f795102bd799b19b5c4172aa7b90
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2023-06-04 19:40:09 -0300

    Addded save to local storage

--- components/ChampionList.tsx
+++ components/ChampionList.tsx
@@ -8,19 +8,31 @@ import {
   HStack,
   Heading,
   Icon,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Stack,
   Tag,
   TagLabel,
   TagRightIcon,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/next-js";
 import { RiotAPITypes } from "@fightmegg/riot-api";
-import { filter, includes, sampleSize, some } from "lodash";
-import { BsDash, BsPlus, BsQuestion } from "react-icons/bs";
-import { LuDices } from "react-icons/lu";
-import Head from "next/head";
-import Link from "next/link";
-
+import { filter, includes, isEqual, sampleSize, set, some } from "lodash";
+import {
+  BsDash,
+  BsDot,
+  BsInfoCircle,
+  BsPlus,
+  BsQuestion,
+  BsXLg,
+} from "react-icons/bs";
+import { LuDelete, LuDices, LuSave } from "react-icons/lu";
+import { v4 as uuidv4 } from "uuid";
 interface ChampionType
   extends RiotAPITypes.DDragon.DDragonChampionListDataDTO {}
 
@@ -40,11 +52,21 @@ interface Option {
   label: string;
 }
 
+interface Composition {
+  id: string;
+  data: {
+    champions: ChampionType[];
+  };
+}
+
 export default function ChampionList({ championsList }: ChampionListProps) {
   const [selectedChamps, setSelectedChamps] = useState<ChampionType[] | null>(
     null
   );
   const [selectedOptions, setSelectedOptions] = useState<Option[] | null>(null);
+  const [savedCompositions, setSavedCompositions] = useState<
+    Composition[] | null
+  >(null);
   const [options, setOptions] = useState<Option[] | null>(
     getChampionsTypeOptions()
   );
@@ -68,9 +90,73 @@ export default function ChampionList({ championsList }: ChampionListProps) {
     setFakeIsLoading(false);
   }
 
+  function loadCompositionsFromLocalStorage() {
+    const localStorageData = localStorage.getItem("compositions") || null;
+    const parsed = !!localStorageData
+      ? (JSON.parse(localStorageData) as Composition[])
+      : null;
+    setSavedCompositions(parsed);
+  }
+
+  function saveCompositionsToLocalStorage(composition: Composition | null) {
+    let isSaveAllowed = true;
+
+    savedCompositions?.forEach((comp) => {
+      if (isEqual(comp?.data.champions, composition?.data.champions)) {
+        isSaveAllowed = false;
+      }
+    });
+
+    if (!isSaveAllowed) {
+      alert("Essa composição já foi salva anteriormente.");
+      return;
+    }
+
+    let upadatedCompositions = [
+      ...new Set([
+        ...(!!savedCompositions ? savedCompositions : []),
+        ...(!!composition ? [composition] : []),
+      ]),
+    ];
+
+    try {
+      localStorage.setItem(
+        "compositions",
+        JSON.stringify(upadatedCompositions)
+      );
+    } catch (error) {
+      alert("Erro ao salver composição!");
+      return;
+    }
+    setSavedCompositions(upadatedCompositions);
+  }
+
+  function deleteCompositionsToLocalStorage(compositionId: string) {
+    const upadatedCompositions =
+      savedCompositions?.filter((item) => item.id !== compositionId) || null;
+    try {
+      localStorage.setItem(
+        "compositions",
+        JSON.stringify(upadatedCompositions)
+      );
+    } catch (error) {
+      alert("Erro ao salver composição!");
+      return;
+    }
+    setSavedCompositions(upadatedCompositions);
+  }
+
   useEffect(() => {
-    FakeLoader();
-  }, [championsList]);
+    loadCompositionsFromLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    console.log("savedCompositions >>> ", savedCompositions);
+  }, [savedCompositions]);
+
+  useEffect(() => {
+    FakeLoader(850);
+  }, []);
 
   function rollChampions() {
     if (
@@ -93,24 +179,6 @@ export default function ChampionList({ championsList }: ChampionListProps) {
     setSelectedChamps(randomChamps);
     return;
   }
-  const filteredChampions = (
-    name: string,
-    champions?: RiotAPITypes.DDragon.DDragonChampionListDataDTO[]
-  ) => {
-    console.log(name);
-    if (!champions || champions.length == 0) {
-      return [] as RiotAPITypes.DDragon.DDragonChampionListDataDTO[];
-    }
-    if (name?.trim().length === 0) {
-      return champions;
-    }
-    return champions?.filter((item) =>
-      item.name.toLocaleLowerCase().includes(name?.trim().toLowerCase())
-    );
-  };
-  const championsTags = new Set(
-    championsList?.map((champ) => champ.tags).flat()
-  );
 
   return (
     <>
@@ -133,7 +201,7 @@ export default function ChampionList({ championsList }: ChampionListProps) {
             />
           ))}
       </Box>
-      <Container zIndex={0} maxW={"3xl"}>
+      <Container pb={10} zIndex={0} maxW={"3xl"}>
         <Stack
           direction={"column"}
           spacing={3}
@@ -187,7 +255,12 @@ export default function ChampionList({ championsList }: ChampionListProps) {
           )}
 
           <Stack alignItems={"center"} gap={3} mt={3} direction={"column"}>
-            <HStack wrap={"wrap"} alignItems={"center"} justifyContent={"center"} spacing={2}>
+            <HStack
+              wrap={"wrap"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              spacing={2}
+            >
               {options?.map((option) => (
                 <Button
                   as={Tag}
@@ -242,16 +315,109 @@ export default function ChampionList({ championsList }: ChampionListProps) {
                 </Button>
               ))}
             </HStack>
-            <Button
-              colorScheme={"messenger"}
-              onClick={rollChampions}
-              rightIcon={<LuDices size={20} />}
-              isLoading={fakeIsLoading}
+            <HStack
+              wrap={"wrap"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              spacing={2}
             >
-              Sortear composição
-            </Button>
+              <Button
+                colorScheme={"gray"}
+                onClick={() => {
+                  if (!selectedChamps) {
+                    alert("Não foi possível salvar a composição");
+                    return;
+                  }
+                  saveCompositionsToLocalStorage({
+                    id: uuidv4(),
+                    data: {
+                      champions: selectedChamps,
+                    },
+                  });
+                }}
+                isLoading={fakeIsLoading}
+              >
+                <LuSave size={20} />
+              </Button>
+
+              <Button
+                colorScheme={"messenger"}
+                onClick={rollChampions}
+                rightIcon={<LuDices size={20} />}
+                isLoading={fakeIsLoading}
+              >
+                Sortear composição
+              </Button>
+            </HStack>
           </Stack>
         </Stack>
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: "xl", sm: "xl", md: "2xl" }}
+          mb={2}
+        >
+          Composições salvas:
+        </Heading>
+
+        <VStack align={"start"} mt={3}>
+          {!!savedCompositions && savedCompositions.length > 0 ? (
+            savedCompositions?.map((composition, index) => (
+              <Stack direction={"row"} align={"center"} key={composition.id}>
+                <Stack
+                  backgroundColor={"red"}
+                  wrap={"wrap"}
+                  direction={"row"}
+                  alignItems={"center"}
+                >
+                  {composition?.data.champions.map((champion) => (
+                    <Box
+                      position={"relative"}
+                      key={champion?.id}
+                      width={["10", "12"]}
+                      height={["10", "12"]}
+                    >
+                      <Image
+                        quality={50}
+                        fill
+                        src={
+                          "http://ddragon.leagueoflegends.com/cdn/" +
+                          champion?.version +
+                          "/img/champion/" +
+                          champion?.image?.full
+                        }
+                        alt={champion?.name}
+                      />
+                    </Box>
+                  ))}
+                </Stack>
+                <Icon
+                  textColor={"gray.400"}
+                  _hover={{ textColor: "gray.700", cursor: "pointer" }}
+                  aria-label="Deletar composição"
+                  onClick={() => {
+                    deleteCompositionsToLocalStorage(composition.id);
+                  }}
+                  as={BsXLg}
+                />
+              </Stack>
+            ))
+          ) : (
+            <HStack
+              width={"full"}
+              backgroundColor={"gray.100"}
+              rounded={"md"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              height={["10", "12"]}
+            >
+              <Icon as={BsInfoCircle} />
+
+              <Text textColor={"gray.600"} textAlign={"center"}>
+                Nada aqui
+              </Text>
+            </HStack>
+          )}
+        </VStack>
       </Container>
     </>
   );
