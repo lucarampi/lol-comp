518600abd1d3f4b59073782146706070ac5ab266
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2023-06-04 02:56:30 -0300

    Url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: !!process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : "http://localhost:3000",
+    baseURL: !!process.env.NEXT_PUBLIC_VERCEL_URL ? `https://lol-comp.vercel.app` : "http://localhost:3000",
 })
 
 export default api;
