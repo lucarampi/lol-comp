75753cda6b8243c93cda2502e26618e2fdbf57e2
Luca Alfaro Rampinelli <lucar@weg.net>
2023-06-19 11:03:26 -0300

    Added open-graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ export const metadata: Metadata = {
   title: "Comp builder",
   description: "Não sabe com o que jogar? Crie sua composição em um click",
   icons: { shortcut: "/gold_icon.png", icon: "/gold_icon.png" },
+  openGraph: { images: "/gold_icon.png" },
 };
 
 export default function RootLayout({
